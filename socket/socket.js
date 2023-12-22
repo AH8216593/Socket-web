019045403a2fd394839d81b495f960acf34ac500
AH8216593 <86178177+AH8216593@users.noreply.github.com>
2023-12-22 11:06:56 -0600

    update to connection user to user

--- socket/socket.js
+++ socket/socket.js
@@ -93,8 +93,8 @@ class Socket {
 					else
 					console.log('mensaje guardado');
             			client.in(data.sala).fetchSockets();
-						// client.broadcast.to(data.sala).emit("recibirMensaje", mensaje, (err, responses) => {
-						client.emit("recibirMensaje", mensaje, (err, responses) => {
+						client.broadcast.to(data.sala).emit("recibirMensaje", mensaje, (err, responses) => {
+						// client.emit("recibirMensaje", mensaje, (err, responses) => {
 							console.log('llego al emit ' + mensaje);
 							console.log("llegaron los  mensajes");
 
