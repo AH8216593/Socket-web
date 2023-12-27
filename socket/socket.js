571c12138914db5f109013de00d06bba9e0337e1
AH8216593 <86178177+AH8216593@users.noreply.github.com>
2023-12-27 16:36:16 -0600

    update to id sala

--- socket/socket.js
+++ socket/socket.js
@@ -139,7 +139,7 @@ class Socket {
 
 			// actualizar status de la sala (mensaje leido)
 			client.on("mensajeLeido", async (data) =>{
-				const getSalas = await services.salas.actualizarStatusSala(data.sala);
+				const getSalas = await services.salas.actualizarStatusSala(data);
 				if(!getSalas)
 						throw new Error(`Error al conseguir las salas: ${JSON.stringify(salas)}`);
 					return (getSalas);
