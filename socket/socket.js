28a2bcd6bb08f9c69a5428ecdc8af9be33338ea5
AH8216593 <86178177+AH8216593@users.noreply.github.com>
2023-12-27 13:22:13 -0600

    udpatye socket

--- socket/socket.js
+++ socket/socket.js
@@ -92,9 +92,9 @@ class Socket {
 						throw new Error(`Error al guardar el mensaje: ${JSON.stringify(mensaje)}`);
 					else
 					console.log('mensaje guardado');
-            			// client.in(data.sala).fetchSockets();
-						client.to(data.sala).emit("recibirMensaje", mensaje, (err, responses) => {
-						// client.broadcast.to(data.sala).emit("recibirMensaje", mensaje, (err, responses) => {
+            			client.in(data.sala).fetchSockets();
+						// client.to(data.sala).emit("recibirMensaje", mensaje, (err, responses) => {
+						client.broadcast.to(data.sala).broadcast.emit("recibirMensaje", mensaje, (err, responses) => {
 						// client.emit("recibirMensaje", mensaje, (err, responses) => {
 							console.log('llego al emit ' + mensaje);
 							console.log("llegaron los  mensajes");
